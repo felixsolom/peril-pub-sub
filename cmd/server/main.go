828adc8bc74f08b4a96e7afa23b0538282f0b42e
felixsolom <felix.solom@gmail.com>
2025-10-22 16:15:33 +0300

    dockerfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	connString := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Printf("Couldn't make a new connection: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("AMQP connetion established")
 	fmt.Println("Starting Peril server...")
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	sig := <-signalChan
+	fmt.Println("Received signal:", sig)
+	fmt.Println("Shutting down gracefully...")
 }
