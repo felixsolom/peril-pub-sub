8d65c6022491801783e141d9c7cec39f90599729
felixsolom <felix.solom@gmail.com>
2025-11-05 21:41:17 +0200

    new handler move

--- cmd/client/handler_move.go
+++ cmd/client/handler_move.go
@@ -5,14 +5,29 @@ import (
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	"github.com/rabbitmq/amqp091-go"
 )
 
-func HandlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) pubsub.Acktype {
+func HandlerMove(gs *gamelogic.GameState, ch *amqp091.Channel) func(gamelogic.ArmyMove) pubsub.Acktype {
 	return func(move gamelogic.ArmyMove) pubsub.Acktype {
 		defer fmt.Print("> ")
+
 		moveOutcome := gs.HandleMove(move)
-		if moveOutcome == gamelogic.MoveOutComeSafe || moveOutcome == gamelogic.MoveOutcomeMakeWar {
+		if moveOutcome == gamelogic.MoveOutComeSafe {
 			return pubsub.Ack
+		}
+		if moveOutcome == gamelogic.MoveOutcomeMakeWar {
+			userName := gs.GetUsername()
+			routingKey := fmt.Sprintf("%s.%s", routing.WarRecognitionsPrefix, userName)
+			err := pubsub.PublishJSON(ch,
+				routing.ExchangePerilTopic,
+				routingKey,
+				moveOutcome)
+			if err != nil {
+				fmt.Printf("Failed to publish war recognition", err)
+			}
+			return pubsub.NackRequeue
 		}
 		return pubsub.NackDiscard
 	}
